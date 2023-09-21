4def32220de882ac4d5021c9252ffb58a12dba8c
aarancis <alexis2396@hotmail.com>
2023-09-21 05:56:46 -0500

    feature/viss-h2b-ms-deploy - modified configurations to deploy in cloud

--- src/main/java/com/bodega/api/BodegaAppApiServiceApplication.java
+++ src/main/java/com/bodega/api/BodegaAppApiServiceApplication.java
@@ -2,10 +2,8 @@ package com.bodega.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@RefreshScope
 @EnableTransactionManagement
 @SpringBootApplication
 public class BodegaAppApiServiceApplication {
